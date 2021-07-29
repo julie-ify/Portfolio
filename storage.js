a362580f8f81f2c98dffcb6bcc70c53a73a4c399
Juliana Ifionu <julieifionu@gmail.com>
2021-07-29 11:40:23 +0100

    Add storage.js file and create function to store user data

---
+++ storage.js
@@ -0,0 +1,28 @@
+const form = document.querySelector('.form');
+const userName = document.getElementById('name');
+const emailId = document.getElementById('email');
+const userMsg = document.getElementById('msg');
+
+if (!localStorage.getItem('name')) {
+  populateStorage();
+} else setStorage();
+
+function populateStorage() {
+  localStorage.setItem('name', document.getElementById('name').value);
+  localStorage.setItem('email', document.getElementById('email').value);
+  localStorage.setItem('msg', document.getElementById('msg').value);
+  setStorage();
+}
+
+function setStorage() {
+  const currentName = localStorage.getItem('name');
+  const currentEmail = localStorage.getItem('email');
+  const currentMsg = localStorage.getItem('msg');
+  document.getElementById('name').value = currentName;
+  document.getElementById('email').value = currentEmail;
+  document.getElementById('msg').value = currentMsg;
+}
+
+userName.onchange = populateStorage;
+emailId.onchange = populateStorage;
+userMsg.onchange = populateStorage;
